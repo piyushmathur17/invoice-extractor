858eed3bce2fa680bc49ff6678f477fe0c3f1d02
“piyushmathur17” <piyushmathur17@gmail.com>
2020-08-04 21:48:09 +0530

    added lines masks

--- rem_lines.py
+++ rem_lines.py
@@ -1,7 +1,6 @@
 import cv2 
-import pytesseract 
 import numpy as np
-
+from graph import make_graph
 def remove_lines(img):
 	gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	laplacian = cv2.Laplacian(gray,cv2.CV_8UC1) # Laplacian Edge Detection
@@ -30,19 +29,25 @@ def remove_lines(img):
 												cv2.CHAIN_APPROX_SIMPLE)
 	#return contours, hierarchy
 
-def ignore_lines(img):
-	#img= cv2.imread('lines_removed0.png')
+
+
+def ignore_lines(img,save_dir,file_name):
+
 	gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	#rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4)) 
 	ret, bw = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
 	ret= min(255,int(1.5*ret))
-	#print(ret)
-	x=bw.copy()
-	y=bw.copy()
-	# Appplying dilation on vertical lines
+	
+	#after thresholding, to connect pixels in weak images
+	structure = cv2.getStructuringElement(cv2.MORPH_RECT, (1,2))
+	x = cv2.dilate(bw, structure,iterations=1) 
+	#structure = cv2.getStructuringElement(cv2.MORPH_RECT, (2,1))
+	#y = cv2.dilate(bw, structure,iterations=1) 
+	y=bw
+
+	# Applying dilation on vertical lines
 	rows = x.shape[0]
 	kernel = np.ones((3,3),np.uint8)
-	vertical_size = rows / 100
+	vertical_size = rows / 80
 	vertical_size=int(vertical_size)
 
 	# Create structure element for extracting vertical lines through morphology operations
@@ -52,22 +57,29 @@ def ignore_lines(img):
 	# Apply morphology operations
 	x = cv2.erode(x, verticalStructure)
 	x = cv2.dilate(x, verticalStructure) 
-	print("writing ...")
-	cv2.imwrite('X.png',x)
-	minLineLength = 200
-	maxLineGap = 20
-	print("extracting vertical lines...",flush=True)
+	cv2.imwrite(save_dir+ 'X_' + file_name,x)
+
+	#applying mask of vertical lines to image
+	rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  
+	x = cv2.dilate(x, rect_kernel, iterations = 1)
+
+	#minLineLength = 200
+	#maxLineGap = 20
+	#print("extracting vertical lines...",flush=True)
 	#ines=[]
-	lines = cv2.HoughLinesP(x,1,np.pi/180,60,minLineLength,maxLineGap)
-	#if lines!=[]:
-	for line in lines:
-	    for x1,y1,x2,y2 in line:
-	        cv2.line(img,(x1,y1),(x2,y2),(ret,ret,ret),3)
+	#lines = cv2.HoughLinesP(x,1,np.pi/180,60,minLineLength,maxLineGap)
+	#garb=0
+	#if lines is not None: 
+	##else:
+	#	for line in lines: 
+	#	    for x1,y1,x2,y2 in line:
+	#	        cv2.line(img,(x1,y1),(x2,y2),(ret,ret,ret),3)
+
 
 	#applying dilation to horizontal lines
 	print("defining horizontal lines...",flush=True)
 	cols = y.shape[1]
-	horizontal_size = cols / 40
+	horizontal_size = cols / 90
 	horizontal_size=int(horizontal_size)
 
 	# Create structure element for extracting horizontal lines through morphology operations
@@ -76,34 +88,42 @@ def ignore_lines(img):
 	# Apply morphology operations
 	y = cv2.erode(y, horizontalStructure)
 	y = cv2.dilate(y, horizontalStructure) 
-	print("writing ...",flush=True)
-	cv2.imwrite('Y.png',y)
 
-	print("extracting horizontal lines...",flush=True)
-	minLineLength = 200
-	maxLineGap = 20
-	lines = cv2.HoughLinesP(y,1,np.pi/180,60,minLineLength,maxLineGap)
-	#lines=[]
-	#if lines != []:
-	for line in lines:
-	    for x1,y1,x2,y2 in line:
-	        cv2.line(img,(x1,y1),(x2,y2),(ret,ret,ret),3)
+	# Applying dilation on horizontal lines
+	rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  
+	y = cv2.dilate(y, rect_kernel, iterations = 1)
 
-	print("writing ...",flush=True)
-	cv2.imwrite('lines_removed.png',img)
-	
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	cv2.imwrite(save_dir+'Y_' + file_name,y)
+
+	#applying vertical and horizontal line masks
+	ret, bw = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU ) 
+	bw = bw + x + y
+
+
+	#print("extracting horizontal lines...",flush=True)
+	#minLineLength = 200
+	#maxLineGap = 20
+	#lines = cv2.HoughLinesP(y,1,np.pi/180,60,minLineLength,maxLineGap)
+	#lines=None
+	#if lines is not None:
+	#	for line in lines:
+	#	    for x1,y1,x2,y2 in line:
+	#	        cv2.line(img,(x1,y1),(x2,y2),(ret,ret,ret),3)
+
+	cv2.imwrite(save_dir+'lines_removed_'+file_name,bw)
+
 	rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)) 
+	
 	# Appplying dilation on the threshold image 
-	ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
+	ret, thresh1 = cv2.threshold(bw, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
 	dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
-	print("writing ...",flush=True)
-	cv2.imwrite('dilated.png', dilation)
+	cv2.imwrite(save_dir+'dilated_'+file_name, dilation)
 	# Finding contours 
 	contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, 
 												cv2.CHAIN_APPROX_SIMPLE)
+
 	print("preprocessing done.",flush=True)
-	return contours,hierarchy
+	return contours,hierarchy, bw
 
 #img = cv2.imread("/home/piyush/Pictures/invoice1.png") 
 #img0 = img.copy()
